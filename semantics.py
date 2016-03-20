44ba3e1f7099cc63152c8949282c5dec09133bbf
Marcel <marcelbm94@gmail.com>
2016-03-20 17:27:38 -0600

    addresses assigned to variables

--- semantics.py
+++ semantics.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import pprint
+from memory import *
 pp = pprint.PrettyPrinter()
 
 ###### Initialize structures ###################################################
@@ -33,6 +34,68 @@ funct_options = {
   'params' : {}
 }
 
+# Memory segment int float  bool  string
+global_segment = [0, 2500, 5000, 7500]
+function_segment = [10000, 12500, 15000, 17500]
+temp_segment = [20000, 22500, 25000, 27500]
+
+def add_global_memory(var_type):
+    if var_type == 'int':
+      if global_segment[0] > 2499:
+        print('Memory overflow')
+        exit(-1)
+      else: 
+        global_segment[0] += 1
+    elif var_type == 'float':
+      if global_segment[1] > 4999:
+        print('Memory overflow')
+        exit(-1)
+      else:
+        global_segment[1] += 1
+    elif var_type == 'bool':
+      if global_segment[2] > 7499:
+        print('Memory overflow')
+        exit(-1)
+      else:
+        global_segment[2] += 1
+    elif var_type == 'string':
+      if global_segment[3] > 9999:
+        print('Memory overflow')
+        exit(-1)
+      else:
+        global_segment[3] += 1
+
+def add_function_memory(var_type):
+  if var_type == 'int':
+    if function_segment[0] > 12499:
+      print('Memory overflow')
+      exit(-1)
+    else: 
+      function_segment[0] += 1
+  elif var_type == 'float':
+    if function_segment[1] > 14999:
+      print('Memory overflow')
+      exit(-1)
+    else:
+      function_segment[1] += 1
+  elif var_type == 'bool':
+    if functionl_segment[2] > 17499:
+      print('Memory overflow')
+      exit(-1)
+    else:
+      function_segment[2] += 1
+  elif var_type == 'string':
+    if function_segment[3] > 19999:
+      print('Memory overflow')
+      exit(-1)
+    else:
+      function_segment[3] += 1
+    
+def add_temp_memory():
+  temp_segment+=1
+
+def realease_temp_memory():
+  temp_segment-=1
 ################################################################################
 
 def print_var_dict():
@@ -82,17 +145,37 @@ def op_error():
 semantic_dict = semantic_dict_constr()
 
 def add_var_to_dict(var_id, var_type, scope):
+  address = assign_address(scope, var_type)
+
   var_dict[scope][var_id] = {
-    'type': types[var_type]
+    'type': types[var_type],
+    'address' : address
   }
   print_var_dict()
 
-# def add_proc_to_dict(proc_id, proc_type, proc_params, proc_address):
-#   proc_dict[proc_id] = {
-#       'type' : proc_type,
-#       'params' : proc_params,
-#       'address' : proc_address
-#   }
+def assign_address(scope, var_type):
+  if scope == 'global':
+    if var_type == 'int':
+      address = global_segment[0]
+    elif var_type == 'float':
+      address = global_segment[1]
+    elif var_type == 'bool':
+      address = global_segment[2]
+    elif var_type == 'string':
+      address = global_segment[3]
+    add_global_memory(var_type)
+  elif scope == 'function':
+    if var_type == 'int':
+      address = function_segment[0]
+    elif var_type == 'float':
+      address = function_segment[1]
+    elif var_type == 'bool':
+      address = function_segment[2]
+    elif var_type == 'string':
+      address = function_segment[3]
+    add_function_memory(var_type)
+  return address
+
 
 def add_funct_to_dict(funct_id, funct_type, funct_params):
   funct_dict[funct_id] = {
